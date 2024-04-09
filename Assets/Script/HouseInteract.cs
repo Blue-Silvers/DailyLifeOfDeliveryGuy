81021734b56a6c98100a69cf2e750997fd178ff8
Pyrocob <pyrocob@gmail.com>
2024-04-09 23:42:49 +0200

    [fix] build fix

--- Assets/Script/HouseInteract.cs
+++ Assets/Script/HouseInteract.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.MaterialProperty;
-using static UnityEditor.Progress;
 
 public class HouseInteract : MonoBehaviour
 {
